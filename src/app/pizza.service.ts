bc84acdd170a8e738375810c88d14ed37ee7807f
tsteelematc <tsteele@madisoncollege.edu>
2026-02-21 11:10:39 -0600

    svc, injections, signal, @for, first display

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core';
 
+  const dummyToppingsFromApi = [
+    {
+      name: "Pepperoni",
+      price: 3.50,
+    },
+    {
+      name: "Sausage",
+      price: 4.25,
+    },
+    {
+      name: "Extra Cheese",
+      price: 0.99,
+    },
+  ];
+
 @Injectable({
   providedIn: 'root',
 })
 export class PizzaService {
-  
+
+  getAvailablePizzaToppings = () => {
+    
+    // Make API call...
+
+    // Transform to shape we need...
+    const pizzaToppingsWithCheckedState = dummyToppingsFromApi.map(
+      x => ({
+        ...x,
+        checked: false,
+      })
+    );
+
+    // Return the transformed pizza toppings...
+    return pizzaToppingsWithCheckedState;
+
+  };
+
 }
