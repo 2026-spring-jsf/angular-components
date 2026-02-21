b1b8a40013129e0ec9cd50770d1d616c0ccd833f
tsteelematc <tsteele@madisoncollege.edu>
2026-02-21 11:11:55 -0600

    currency pipe, neat

--- src/app/pizza-toppings/pizza-toppings.ts
+++ src/app/pizza-toppings/pizza-toppings.ts
@@ -1,9 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { PizzaService } from '../pizza.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-pizza-toppings',
-  imports: [],
+  imports: [
+    CurrencyPipe,
+  ],
   templateUrl: './pizza-toppings.html',
   styleUrl: './pizza-toppings.css',
 })
