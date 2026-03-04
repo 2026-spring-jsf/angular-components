6b25a28e36418b43b22735497dd7c2cd3ad5eebf
DYZ-1234 <drewweise@gmail.com>
2026-03-04 11:49:08 -0600

    checkbox bound to signal data

--- src/app/pizza-toppings/pizza-toppings.ts
+++ src/app/pizza-toppings/pizza-toppings.ts
@@ -1,5 +1,5 @@
 import { Component, inject, signal } from '@angular/core';
-import { PizzaService } from '../pizza.service';
+import { PizzaService, PizzaTopping } from '../pizza.service';
 import { CurrencyPipe } from '@angular/common';
 
 @Component({
@@ -12,4 +12,9 @@ export class PizzaToppings {
   private readonly pizzaSvc = inject(PizzaService);
 
   protected readonly availablePizzaToppings = signal(this.pizzaSvc.getAvailablePizzaToppings());
+
+  protected readonly toggleTopping = (t: PizzaTopping) => 
+    this.availablePizzaToppings.update(prev => 
+      prev.map(x => x === t ? {...x, checked: !x.checked} : x)
+    );
 }
