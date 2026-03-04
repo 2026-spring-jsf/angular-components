8f066c91f728291e3638d7ca657ffd4b0f51e3c0
Michael Rooney <msroone@gmail.com>
2026-03-03 20:22:14 -0600

    hooked up computed signal for totals

--- src/app/pizza-toppings/pizza-toppings.ts
+++ src/app/pizza-toppings/pizza-toppings.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { PizzaService, PizzaTopping } from '../pizza.service';
 import { CurrencyPipe } from '@angular/common'; 
 
@@ -10,6 +10,16 @@ import { CurrencyPipe } from '@angular/common';
 })
 export class PizzaToppings {
   private readonly pizzaSvc = inject(PizzaService);
+
   protected readonly availablePizzaToppings = signal(this.pizzaSvc.getToppings());
+
+protected readonly total = computed(
+  () => this.availablePizzaToppings()
+  .filter
+  (x => x.checked)
+    .reduce((acc, x) => acc + x.price,
+     0
+    )
+  );
 
   protected readonly toggleTopping = (t: PizzaTopping) => this.availablePizzaToppings.update(prev => prev.map(x => x === t ? { ...x, checked: !x.checked } : x));}
