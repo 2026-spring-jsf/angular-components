a3b62adf54dc623e8716d330235bf7f77a9457bf
tsteelematc <tsteele@madisoncollege.edu>
2026-03-06 14:43:21 -0600

    ng generate component tsteele-addition --skip-tests

---
+++ src/app/tsteele-addition/tsteele-addition.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tsteele-addition',
+  imports: [],
+  templateUrl: './tsteele-addition.html',
+  styleUrl: './tsteele-addition.css',
+})
+export class TsteeleAddition {
+
+}
