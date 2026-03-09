2c1a55798fd90e06edf355ea39a654d68b1d5788
Colin Bares <colinedwinbares@gmail.com>
2026-03-09 02:56:20 -0500

    Create array to store results and dice elements

--- src/app/cbares-dice-roll/cbares-dice-roll.ts
+++ src/app/cbares-dice-roll/cbares-dice-roll.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -12,6 +12,9 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './cbares-dice-roll.css',
 })
 export class CbaresDiceRoll {
+
+  protected readonly results = signal([0,0]);
+
   protected readonly rollDice = () => {
     console.log('roll button clicked');
   }
