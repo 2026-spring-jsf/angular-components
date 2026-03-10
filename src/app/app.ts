a6b62aa6014576197f0ab855c9884dd49ac56e2f
THamilton1123 <thamilton12@madisoncollege.edu>
2026-03-10 18:36:24 -0500

    Showing my component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { PizzaToppings } from './pizza-toppings/pizza-toppings';
+import { ThamiltonRollD20 } from './thamilton-roll-d20/thamilton-roll-d20';
 
 @Component({
   selector: 'app-root',
-  imports: [PizzaToppings],
+  imports: [PizzaToppings, ThamiltonRollD20],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-components');
