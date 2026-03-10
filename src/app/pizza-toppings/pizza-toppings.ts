95e6203eb91a381b0518ddc4900834b93522cd39
THamilton1123 <thamilton12@madisoncollege.edu>
2026-03-10 17:31:18 -0500

    Styled the component to be on a card, not bad...

--- src/app/pizza-toppings/pizza-toppings.ts
+++ src/app/pizza-toppings/pizza-toppings.ts
@@ -1,43 +1,34 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { PizzaService, PizzaTopping } from '../pizza.service';
 import { CurrencyPipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-pizza-toppings',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, MatCardModule],
   templateUrl: './pizza-toppings.html',
   styleUrl: './pizza-toppings.css',
 })
 export class PizzaToppings {
+  private readonly pizzaSvc = inject(PizzaService);
 
-  private readonly pizzaSvc = inject(
-    PizzaService
+  protected readonly availablePizzaToppings = signal(this.pizzaSvc.getAvailablePizzaToppings());
+
+  protected readonly total = computed(() =>
+    this.availablePizzaToppings()
+      .filter((x) => x.checked)
+      .reduce((acc, x) => acc + x.price, 0),
   );
 
-  protected readonly availablePizzaToppings = signal(
-    this.pizzaSvc.getAvailablePizzaToppings()
-  );
-
-  protected readonly total = computed(
-    () => this.availablePizzaToppings()
-      .filter(
-        x => x.checked
-      )
-      .reduce(
-        (acc, x) => acc + x.price,
-        0,
-      )
-  );
-
-  protected readonly toggleTopping = (t: PizzaTopping) => this.availablePizzaToppings.update(
-    prev => prev.map(
-      x => x === t
-        ? {
-          ...x,
-          checked: !x.checked
-        }
-        : x
-    )
-  );
-
+  protected readonly toggleTopping = (t: PizzaTopping) =>
+    this.availablePizzaToppings.update((prev) =>
+      prev.map((x) =>
+        x === t
+          ? {
+              ...x,
+              checked: !x.checked,
+            }
+          : x,
+      ),
+    );
 }
