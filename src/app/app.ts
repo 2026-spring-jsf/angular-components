2f5c1fdd2efd255821e37b090b0d0ef08b629c19
lmeier127 <Lmeier127@gmail.com>
2026-03-15 14:08:06 -0500

    Added placeholders for custom topping input and list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { PizzaToppings } from './pizza-toppings/pizza-toppings';
+import { LmeierCustomTopping } from './lmeier-custom-topping/lmeier-custom-topping';
 
 @Component({
   selector: 'app-root',
-  imports: [PizzaToppings],
+  imports: [
+    PizzaToppings,
+    LmeierCustomTopping,
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/lmeier-custom-topping/lmeier-custom-topping.ts
+++ src/app/lmeier-custom-topping/lmeier-custom-topping.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-lmeier-custom-topping',
-  imports: [],
+  imports: [
+    MatCardModule
+  ],
   templateUrl: './lmeier-custom-topping.html',
   styleUrl: './lmeier-custom-topping.css',
 })
