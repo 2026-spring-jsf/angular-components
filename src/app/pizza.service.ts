2cf74cc7755a4fe029fac143f83043a04782fd0b
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-03-14 23:39:48 -0500

    service generated with added getPizzaToppings method

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
 
+const PIZZA_TOPPINGS = [
+  {
+    name: "Pepperoni",
+    price: 3.50,
+  },
+  {
+    name: "Sausage",
+    price: 4.25,
+  },
+  {
+    name: "Extra Cheese",
+    price: 0.99,
+  }
+];
+
 @Injectable({
   providedIn: 'root',
 })
 export class PizzaService {
-  
+  getPizzaToppings = () => PIZZA_TOPPINGS.map(topping => ({...topping, checked: false,}));
 }
