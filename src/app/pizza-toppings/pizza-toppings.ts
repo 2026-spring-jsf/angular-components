266123c19c9182494d0f579b579b1196c0a7be02
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-03-15 00:43:46 -0500

    Checkbox bound to signal data

--- src/app/pizza-toppings/pizza-toppings.ts
+++ src/app/pizza-toppings/pizza-toppings.ts
@@ -1,5 +1,5 @@
 import { Component, inject, signal } from '@angular/core';
-import { PizzaService } from '../pizza.service';
+import { PizzaService, PizzaTopping } from '../pizza.service';
 import { CurrencyPipe } from '@angular/common';
 
 @Component({
@@ -8,7 +8,14 @@ import { CurrencyPipe } from '@angular/common';
   templateUrl: './pizza-toppings.html',
   styleUrl: './pizza-toppings.css',
 })
+
 export class PizzaToppings {
   private readonly pizzaSvc = inject(PizzaService);
   protected readonly pizzaToppings = signal(this.pizzaSvc.getPizzaToppings());
+
+  protected readonly toggleTopping = (topping_to_update: PizzaTopping) => this.pizzaToppings.update(
+    topping => topping.map(
+      topping => topping === topping_to_update ? {...topping, checked: !topping.checked,} : topping
+    )
+  );
 }
