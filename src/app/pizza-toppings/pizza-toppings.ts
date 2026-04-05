32d4876704fb6bfc2f8c5b640ca9a25b5a1dd0d4
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-04-05 18:50:01 -0500

    Added styled buttons

--- src/app/pizza-toppings/pizza-toppings.ts
+++ src/app/pizza-toppings/pizza-toppings.ts
@@ -2,10 +2,11 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { PizzaService, PizzaTopping } from '../pizza.service';
 import { CurrencyPipe } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-pizza-toppings',
-  imports: [CurrencyPipe, MatCardModule],
+  imports: [CurrencyPipe, MatButtonModule, MatCardModule],
   templateUrl: './pizza-toppings.html',
   styleUrl: './pizza-toppings.css',
 })
