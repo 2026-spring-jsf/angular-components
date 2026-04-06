5683c7c1887ee758326501ccdcaa237c374bbde1
Kelsang Norbu <knorbu@edgewood.edu>
2026-04-06 07:56:36 -0500

    ng generate component

---
+++ src/app/knorbu-taxes/knorbu-taxes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-knorbu-taxes',
+  imports: [],
+  templateUrl: './knorbu-taxes.html',
+  styleUrl: './knorbu-taxes.css',
+})
+export class KnorbuTaxes {
+
+}
