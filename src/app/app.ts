3a5befb8ed4e70bc3059bffb2749c16344289a23
jfr-0099 <jfrederick2@madisoncollege.edu>
2026-04-14 21:32:36 +0000

    Fixed order on app ts & httml

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,6 @@ import { Component, signal } from '@angular/core';
 import { PizzaToppings } from './pizza-toppings/pizza-toppings';
 import { TsteeleMultiplication } from './tsteele-multiplication/tsteele-multiplication';
 import { CbaresDiceRoll } from './cbares-dice-roll/cbares-dice-roll';
-import { Jfr0099Magic8Ball } from './jfr-0099-magic-8-ball/jfr-0099-magic-8-ball';
 import { ZsmuckerbryanChecklist } from './zsmuckerbryan-checklist/zsmuckerbryan-checklist';
 import { MrooneyAddition } from './mrooney-addition/mrooney-addition';
 import { LmeierCustomTopping } from './lmeier-custom-topping/lmeier-custom-topping';
@@ -10,6 +9,7 @@ import { ThamiltonRollD20 } from './thamilton-roll-d20/thamilton-roll-d20';
 import { DweiseClickCounter } from './dweise-click-counter/dweise-click-counter'
 import { KnorbuTaxes } from './knorbu-taxes/knorbu-taxes';
 import { CsniderIncrementer } from "./csnider-incrementer/csnider-incrementer";
+import { Jfr0099Magic8Ball } from './jfr-0099-magic-8-ball/jfr-0099-magic-8-ball';
 
 @Component({
   selector: 'app-root',
@@ -18,13 +18,13 @@ import { CsniderIncrementer } from "./csnider-incrementer/csnider-incrementer";
     TsteeleMultiplication,
     CbaresDiceRoll,
     ZsmuckerbryanChecklist,
-    Jfr0099Magic8Ball,
     MrooneyAddition,
     LmeierCustomTopping,
     ThamiltonRollD20,
     DweiseClickCounter,
     KnorbuTaxes,
-    CsniderIncrementer
+    CsniderIncrementer,
+    Jfr0099Magic8Ball
   ],
   templateUrl: './app.html',
   styleUrl: './app.css',
