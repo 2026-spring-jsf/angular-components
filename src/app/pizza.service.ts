1c27801401efa4176fa7955ed6a108c541b58f8c
emmajonesfrontend <emma.jones.frontend@gmail.com>
2026-04-15 13:57:28 -0500

    a slightly better service method/func
    with a type definition

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
-
+export type PizzaTopping = {
+  name: string;
+  price: number;
+  checked: boolean;
+};
 const dummyToppingsFromApi = [
   {
-  name: 'Pepperroni',
-  price: 3.50,
-  },
-{
-  name: 'Saussage',
-  price: 4.25,
+    name: 'Pepperroni',
+    price: 3.5,
   },
   {
-  name: 'Extra Cheese',
-  price: 0.99,
+    name: 'Saussage',
+    price: 4.25,
+  },
+  {
+    name: 'Extra Cheese',
+    price: 0.99,
   },
 ];
 
@@ -19,13 +23,16 @@ const dummyToppingsFromApi = [
   providedIn: 'root',
 })
 export class PizzaService {
-
   // making a map
-  getAvailablePizzaToppings = () => dummyToppingsFromApi.map(
-    x => ({
+  getAvailablePizzaToppings = (): PizzaTopping[] => {
+    // Make the API Call to get the pizz toppings
+
+    // Shape them for use in the app..
+    const pizzaToppingsShaedForTheApp = dummyToppingsFromApi.map((x) => ({
       ...x,
-      checked: false;
-    })
-  );
-  
+      checked: false,
+    }));
+    // return the shpaed pizza toppings..
+    return pizzaToppingsShaedForTheApp;
+  };
 }
