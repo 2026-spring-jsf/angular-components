66c9663337e197b87294152e03dd3277d37d8f7d
Shaun Khang <khaunx02@gmail.com>
2026-04-29 17:03:01 -0500

    Showing my pizza topping component!!!

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PizzaToppings } from './pizza-toppings/pizza-toppings';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PizzaToppings],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-components');
